5b82ce2df19ddc9d5de6fc00fc385edff88c94ae
Graham Paterson <graywinpat@gmail.com>
2014-04-19 13:41:13 +0100

    Planting Added

--- farming.js
+++ farming.js
@@ -35,6 +35,49 @@ var playerObj = {
     currentCrop: 0             
 }
 
+gameObj.crops = [
+    {
+        name: 'tomato',
+        cost: 10,
+        revenue: 18,
+        time_to_ripe: 10, //secods
+        time_to_death: 30, //second from when it's ripe
+        image: 'tomato.png'
+    },
+    {
+        name: 'artichoke',
+        cost: 20,
+        revenue: 38,
+        time_to_ripe: 60,
+        time_to_death: 60,
+        image: 'artichoke.png'
+    },
+    {
+        name: 'lettuce',
+        cost: 15,
+        revenue: 26,
+        time_to_ripe: 30,
+        time_to_death: 60,
+        image: 'lettuce.png'
+    },
+    {
+        name: 'eggplant',
+        cost: 30,
+        revenue: 78,
+        time_to_ripe: 120,
+        time_to_death: 120,
+        image: 'eggplant.png'
+    },
+    {
+        name: 'peppers',
+        cost: 40,
+        revenue: 82,
+        time_to_ripe: 180,
+        time_to_death: 180,
+        image: 'peppers.png'
+    }
+];
+
 
     var director = new lime.Director(document.body,gameObj.width,gameObj.height);     
     director.makeMobileWebAppCapable();     
@@ -65,6 +108,12 @@ var moneyLabel = new lime.Label().setText('$'+playerObj.money).setFontColor('#E8
     .setPosition(gameObj.controlsLayer_w-50, gameObj.height-gameObj.controlsLayer_h/2);
 controlsLayer.appendChild(moneyLabel); 
 
+//updating money indicator
+gameObj.updateMoney = function() {
+    moneyLabel.setText('$'+playerObj.money);
+};
+
+
  //create land elements
     for(var i=0; i<gameObj.num_tiles_x; i++) {
         for(var j=0; j<gameObj.num_tiles_y; j++) {
@@ -77,8 +126,60 @@ controlsLayer.appendChild(moneyLabel);
 
     director.replaceScene(gameScene); 
 
-   
+
+
+   //shop
+var shopScene = new lime.Scene().setRenderer(lime.Renderer.CANVAS);
+var shopLayer = new lime.Layer().setAnchorPoint(0, 0);
+
+var shopBackground = new lime.Sprite().setAnchorPoint(0,0).setPosition(0,0)
+    .setSize(gameObj.width, gameObj.height).setFill('#777777');
+shopLayer.appendChild(shopBackground);
+shopScene.appendChild(shopLayer);
+
+//close button
+var closeButton = new lime.GlossyButton().setColor('#ffffff').setText('Back')
+    .setPosition(gameObj.width/2, gameObj.height-25)
+    .setSize(80, 40);
+shopLayer.appendChild(closeButton);
+
+//launch shop event
+goog.events.listen(shopButton,['mousedown', 'touchstart'], function(e) {
+    director.replaceScene(shopScene);
+});
+
+//close shop event
+goog.events.listen(closeButton,['mousedown', 'touchstart'], function(e) {
+    director.replaceScene(gameScene);
+});
+
+//shop items
+for(var i=0; i<gameObj.crops.length; i++) {
+    var item = new lime.Sprite().setAnchorPoint(0,0).setPosition(gameObj.shop_margin_x, gameObj.shop_margin_y + (gameObj.shop_margin_y + gameObj.tile_size)*i)
+        .setFill('images/'+gameObj.crops[i].image).setSize(gameObj.tile_size, gameObj.tile_size);
+    shopLayer.appendChild(item);
+
+    var timeLabel = new lime.Label().setText(gameObj.crops[i].name+' ('+gameObj.crops[i].time_to_ripe+' days)').setFontColor('#E8FC08')
+    .setPosition(gameObj.shop_margin_x+150, gameObj.shop_margin_y*1.5 + (gameObj.shop_margin_y + gameObj.tile_size)*i);
+    shopLayer.appendChild(timeLabel);
+    var costLabel = new lime.Label().setText('cost: $'+gameObj.crops[i].cost).setFontColor('#E8FC08')
+    .setPosition(gameObj.shop_margin_x+150, gameObj.shop_margin_y*2.5 + (gameObj.shop_margin_y + gameObj.tile_size)*i);
+    shopLayer.appendChild(costLabel);
+    var label = new lime.Label().setText('revenue: $'+gameObj.crops[i].revenue).setFontColor('#E8FC08')
+    .setPosition(gameObj.shop_margin_x+150, gameObj.shop_margin_y*3.4 + (gameObj.shop_margin_y + gameObj.tile_size)*i);
+    shopLayer.appendChild(label);
+
+    //pick crop
+    (function(item, i) {
+        goog.events.listen(item,['mousedown', 'touchstart'], function(e) {
+            playerObj.currentCrop = i;
+            director.replaceScene(gameScene);
+        });
+    })(item, i);
+}
 
 }
+
+
 
 
--- land.js
+++ land.js
@@ -24,8 +24,48 @@ farming.Land = function(gameObj, playerObj) {
             playerObj.money -= gameObj.costPlowing;
             gameObj.updateMoney();
         }
+        else if(land.state == land.PLOWED && playerObj.money >= gameObj.crops[playerObj.currentCrop].cost) {
+        //plant
+        land.setFill('images/growing.png');
+        land.state = land.GROWING;
+
+        //store crop and left time for it to be ready and to die
+        land.crop = playerObj.currentCrop;
+        land.ripeTime = gameObj.crops[playerObj.currentCrop].time_to_ripe * 1000;
+        land.deathTime = gameObj.crops[playerObj.currentCrop].time_to_death * 1000;
+
+        //update player money
+        playerObj.money -= gameObj.crops[playerObj.currentCrop].cost;
+        gameObj.updateMoney();
+    }
+
+    //growing plants
+dt = 1000;
+lime.scheduleManager.scheduleWithDelay(function() {
+    if(this.state == land.GROWING) {            
+        if(this.ripeTime <= 0) {
+            this.state = land.READY;
+            this.setFill('images/'+gameObj.crops[this.crop].image);
+        }
+        else {
+            this.ripeTime -= dt;
+        }
+    }
+    else if(this.state == land.READY) {
+        if(this.deathTime <= 0) {
+            this.state = land.EMPTY;
+            this.setFill('images/bare_land.png');
+        }
+        else {
+            this.deathTime -= dt;
+        }
+    }
+}, this, dt);
+
     });
 }
+
+
 
 goog.inherits(farming.Land,lime.Sprite);
 
